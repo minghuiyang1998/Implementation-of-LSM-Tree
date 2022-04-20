86f74f9fd4fccc0e0ced23bd02b111eaf4dc9140
Yang Minghui <minghuiyang1998@gmail.com>
2022-04-19 23:51:53 -0400

    update compact with infinite levels and calculated threshold

--- src/Levels/Levels.cpp
+++ src/Levels/Levels.cpp
@@ -2,9 +2,8 @@
 
 Levels::Levels() {}
 
-Levels::Levels(int size) {
-    totalSize = size;
-    levelVector = vector<Level>(size);
+Levels::Levels(int firstLevelThreshold) {
+    this -> firstLevelThreshold = firstLevelThreshold;
 }
 
 Levels::~Levels() {}
@@ -22,5 +21,13 @@ void Levels::setLevelVector(int l, const Level& level) {
 }
 
 int Levels::getTotalSize() const {
-    return totalSize;
+    return levelVector.size();
+}
+
+void Levels::addALevel() {
+    int newLevelNumb = levelVector.size();
+    // T, T^2, T^3, T^4.....
+    int newThreshold = pow(firstLevelThreshold, newLevelNumb);
+    Level newLevel(newLevelNumb, newThreshold);
+    levelVector.push_back(newLevel);
 }
