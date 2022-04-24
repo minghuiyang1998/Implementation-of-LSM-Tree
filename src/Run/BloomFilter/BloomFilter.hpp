2d57b5fad7a98b44920b1d61f24c06c5fab0be68
Yang Minghui <minghuiyang1998@gmail.com>
2022-04-23 20:46:39 -0400

    fix bloomfilter

--- src/Run/BloomFilter/BloomFilter.hpp
+++ src/Run/BloomFilter/BloomFilter.hpp
@@ -8,33 +8,20 @@ namespace BF {
     class BloomFilter {
     public:
         BloomFilter();
-
         BloomFilter(int numElement_, int bitsPerElement_);
-
         BloomFilter( int numElement_, int bitsPerElement_, vector<bool> bf_vec );
-
         int numElement;
         int bitsPerElement;
-
         void program(string key);
-
         bool query(string key);
-
         int getIndexNum();
-
         int getSize();
-
+        const vector<bool> &getBfVec() const;
     private:
         int numIndex;
         int size;
-    public:
-        const vector<bool> &getBfVec() const;
-
-    private:
         vector<bool> bf_vec;
-
         void makeBloomFilter();
-
         void getIndex(string key, vector<int> *index);
     };
 }
