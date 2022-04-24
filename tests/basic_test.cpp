a135ffce68893302f1c4a2cad61d248a02dc0789
Yang Minghui <minghuiyang1998@gmail.com>
2022-04-23 21:04:12 -0400

    Each test automatically shuts down the database

--- tests/basic_test.cpp
+++ tests/basic_test.cpp
@@ -27,57 +27,75 @@ protected:
         db1.put(2, v1);
         db1.put(5, v2);
         db2.put(1024, v3);
+
+        db0.close();
+        db1.close();
+        db2.close();
     }
 };
 
 
 TEST_F(DBTest, IsEmptyInitially)
 {
+    db0.open("basic_test_db0.txt");
     EXPECT_EQ(db0.size(), 0);
+    db0.close();
 }
 
 
 TEST_F(DBTest, GetFunctionality)
 {
+    db1.open("basic_test_db1.txt");
     Value val = db1.get(2);
     EXPECT_EQ(DBTest::v1, val);
+    db1.close();
 }
 
 
 TEST_F(DBTest, PutAndGetFunctionality)
 {
+    db0.open("basic_test_db0.txt");
     db0.put(10, v4);
     Value get_value = db0.get(10);
 
     EXPECT_EQ(DBTest::v4, get_value);
+    db0.close();
 }
 
 
 TEST_F(DBTest, DeleteFunctionality)
 {
+    db1.open("basic_test_db1.txt");
     db1.del(2);
     EXPECT_EQ(db1.get(2), Value(false));
     EXPECT_EQ(db1.size(), 1);
 
+    db2.open("basic_test_db2.txt");
     db2.del(1024);
     EXPECT_EQ(db2.get(1024), Value(false));
     EXPECT_EQ(db2.size(), 0);
+    db1.close();
+    db2.close();
 }
 
 
 TEST_F(DBTest, ScanFunctionality)
 {
+    db2.open("basic_test_db2.txt");
     std::vector<Value> vals;
     vals = db2.scan();
     ASSERT_EQ(vals.size(), 1);
     EXPECT_EQ(vals[0], DBTest::v3);
 
+    db1.open("basic_test_db1.txt");
     vals = db1.scan(1, 3);
     ASSERT_EQ(vals.size(), 1);
     EXPECT_EQ(vals[0], DBTest::v1);
 
     vals = db1.scan();
     ASSERT_EQ(vals.size(), 2);
+    db1.close();
+    db2.close();
 }
 
 
